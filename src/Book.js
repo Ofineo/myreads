1b71427195ad615fc9971ebdca20e11f42b93bdb
Jordi <ofineo@gmail.com>
2018-02-21 21:25:04 +0000

    got books from API and arranged on shelves

--- src/Book.js
+++ src/Book.js
@@ -5,14 +5,20 @@ import SelectShelf from './SelectShelf'
 class Book extends Component {
     render() {
         return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <SelectShelf />
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-            </div>
+            <ol className="books-grid">
+                {this.props.allBooks.filter(books=> books.shelf === this.props.shelf ).map(book => (
+                    <li>
+                        <div className="book">
+                            <div className="book-top">
+                                <img src={book.imageLinks.smallThumbnail} />
+                                <SelectShelf />
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                        </div>
+                    </li>
+                ))}
+            </ol>
         )
     }
 }
